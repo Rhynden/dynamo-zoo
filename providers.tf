0fa1ba4d2edfdcbb5ffc4c5a5a46cef8a8e64b8b
Florian B <florian_boeck@hotmail.de>
2023-12-28 11:24:57 +0100

    Add providers.tf

---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~> 1.6.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # Optionally store tf state in aws s3 bucket
+  #  backend "s3" {
+  #   bucket = "yourdomain-terraform"
+  #   key    = "prod/terraform.tfstate"
+  #   region = "eu-central-1"
+  # }
+}
+
+# Default region to use
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Provider for Cloudfront etc.
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+}
