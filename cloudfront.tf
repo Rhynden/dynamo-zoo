6417ccc3e16f13ba157f914b4d4108e545d68953
Florian B <florian_boeck@hotmail.de>
2023-12-30 17:32:50 +0100

    Add static website served by s3+cloudfront

---
+++ cloudfront.tf
@@ -0,0 +1,45 @@
+resource "aws_cloudfront_origin_access_control" "static_website_oac" {
+  name                              = "example"
+  description                       = "Example Policy"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "static_website_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_website_bucket.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.static_website_oac.id
+    origin_id                = local.s3_origin_id
+  }
+
+  aliases = [var.domain_name]
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    # Using the CachingDisabled managed policy ID:
+    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id       = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.acm_certificate.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
---
+++ s3.tf
@@ -0,0 +1,48 @@
+// Bucket for the staitc website files
+resource "aws_s3_bucket" "static_website_bucket" {
+  bucket = "fb-dynamozoo-static-website-bucket"
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.static_website_bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket       = aws_s3_bucket.static_website_bucket.id
+  key          = "error.html"
+  source       = "error.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_policy" "s3_bucket_policy_cloudfront" {
+  bucket = aws_s3_bucket.static_website_bucket.id
+  policy = data.aws_iam_policy_document.iam_policy_document_allow_cloudfront.json
+}
+
+data "aws_iam_policy_document" "iam_policy_document_allow_cloudfront" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.static_website_bucket.arn}/*",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = ["${aws_cloudfront_distribution.static_website_distribution.arn}"]
+    }
+  }
+}
