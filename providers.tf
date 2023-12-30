8693719082ff1746222b8d7f5033f522d044f1dd
Florian B <florian_boeck@hotmail.de>
2023-12-30 18:16:24 +0100

    Add tf archive to providers

--- providers.tf
+++ providers.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.1"
+    }
   }
 
   # Optionally store tf state in aws s3 bucket
