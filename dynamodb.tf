e396230fd45f0abdbf4f8d6dfb075adc77f6879f
Florian B <florian_boeck@hotmail.de>
2024-01-02 13:29:06 +0100

    Add items to animals table

--- dynamodb.tf
+++ dynamodb.tf
@@ -17,3 +17,30 @@ resource "aws_dynamodb_table" "s3_imagepath_table" {
     environment = "${var.environment}"
   }
 }
+
+// DynamoDB items
+resource "aws_dynamodb_table_item" "dog1" {
+  table_name = aws_dynamodb_table.s3_imagepath_table.name
+  hash_key   = aws_dynamodb_table.s3_imagepath_table.hash_key
+  range_key  = aws_dynamodb_table.s3_imagepath_table.range_key
+
+  item = <<ITEM
+{
+  "animal_name": {"S": "dog"},
+  "image_uri": {"S": "www.s3.de/dog1"}
+}
+ITEM
+}
+
+resource "aws_dynamodb_table_item" "dog2" {
+  table_name = aws_dynamodb_table.s3_imagepath_table.name
+  hash_key   = aws_dynamodb_table.s3_imagepath_table.hash_key
+  range_key  = aws_dynamodb_table.s3_imagepath_table.range_key
+
+  item = <<ITEM
+{
+  "animal_name": {"S": "dog"},
+  "image_uri": {"S": "www.s3.de/dog2"}
+}
+ITEM
+}
