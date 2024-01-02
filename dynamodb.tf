972db935ed6f8dbb35685832df371e630cc03d32
Florian B <florian_boeck@hotmail.de>
2024-01-02 13:28:51 +0100

    Add image_uri to animals table

--- dynamodb.tf
+++ dynamodb.tf
@@ -4,8 +4,13 @@ resource "aws_dynamodb_table" "s3_imagepath_table" {
   name         = var.table_name
   billing_mode = var.table_billing_mode
   hash_key     = "animal_name"
+  range_key    = "image_uri"
   attribute {
     name = "animal_name"
+    type = "S"
+  }
+  attribute {
+    name = "image_uri"
     type = "S"
   }
   tags = {
