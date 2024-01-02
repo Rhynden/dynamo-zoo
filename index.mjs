8f8dea300925538f4d7d59611a9f30b33c747e98
Florian B <florian_boeck@hotmail.de>
2024-01-02 13:38:11 +0100

    Add lambda code to query dyamodb

--- index.mjs
+++ index.mjs
@@ -1,15 +1,56 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
+
+const dynamo = DynamoDBDocument.from(new DynamoDB());
+
 console.log('Loading function');
 
 export const handler = async (event, context) => {
-    const response = {
-        statusCode: '200',
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "*"
-        },
-        body: 'Hello world from my lambda',
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*"
     };
 
-    return response;
+    try {
+        switch (event.httpMethod) {
+            // case 'DELETE':
+            //     body = await dynamo.delete(JSON.parse(event.body));
+            //     break;
+            case 'GET':
+                const params = {
+                    KeyConditionExpression : "animal_name = :dog",
+                    ExpressionAttributeValues: {
+                        ':dog': "dog"
+                    },
+                    TableName: "animals"
+                }
+                body = await dynamo.query(params);
+                break;
+            // case 'POST':
+            //     body = await dynamo.put(JSON.parse(event.body));
+            //     break;
+            // case 'PUT':
+            //     body = await dynamo.update(JSON.parse(event.body));
+            //     break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
 };
